6481c81f0e45800b3fe8bfff48c0286360c03a71
56quique <enriqueklenser241@gmail.com>
2026-03-25 20:43:06 -0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,11 +1,12 @@
-import { crearGauge } from "/js/gauge.js";
+import { crearGauge } from "./gauge.js";
 
 const g1 = crearGauge("vL1", 0, 300);
 const g2 = crearGauge("vL2", 0, 300);
 const g3 = crearGauge("vL3", 0, 300);
 
+// Simulación con variación realista
 setInterval(() => {
-  g1.set(220);
-  g2.set(230);
-  g3.set(210);
+  g1.set(210 + Math.random() * 30);
+  g2.set(220 + Math.random() * 20);
+  g3.set(200 + Math.random() * 40);
 }, 1000);
